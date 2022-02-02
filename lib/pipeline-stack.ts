51decda8ad60b4fe528124a46503620e0b604b53
ishikabhardwaj <ishikabhardwaj7601@gmail.com>
2022-02-02 07:21:42 +0530

    updated commands under pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -10,7 +10,7 @@ export class PipelineStack extends cdk.Stack {
       pipelineName: 'DhaniPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+        commands: ['npx cdk synth']
       })
     });
   }
